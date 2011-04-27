5f9c5b0a5f871389c85b5b1de4abbb449ffeb28d
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2011-04-27 02:30:43 +0200

    moved ingredients to different file, added "feeling lucky" button

---
+++ ingredients.fr.js
@@ -0,0 +1,54 @@
+var veggies = [
+  "Tomate", 
+  "Avocat",
+  "Mangue", 
+  "Poivron", 
+  "Tomate séchée", 
+  "Carotte", "Maïs", 
+  "Pois chiches", 
+  "Endive", 
+  "Haricots rouges", 
+  "Betterave", 
+  "Champignons"
+];
+
+var cheese = [
+  "Feta", 
+  "Gruyère", 
+  "Emmental", 
+  "Chèvre", 
+  "Luzerner", 
+  "Roquefort", 
+  "Mozzarella"
+];
+
+var proteins = [
+  "Saucisse", 
+  "Bacon", 
+  "Thon", 
+  "Œuf", 
+  "Poulet", 
+  "Nuggets", 
+  "Boulettes", 
+  "Saumon", 
+  "Jambon", 
+  "Thon", 
+  "Tofu", 
+  "Canard séché", 
+  "Gésiers", 
+  "Jambon cru"
+];
+
+var extra = [
+  "Croûtons", 
+  "Œuf dur", 
+  "Pignons"
+];
+
+var base = [
+  "Salade", 
+  "Pâtes", 
+  "Riz", 
+  "Blé", 
+  "Pommes de terre"
+]
