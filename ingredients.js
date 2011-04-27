eb67424ca2bbd7565b24dc8a892066c0ed2c8dc1
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2011-04-27 07:13:00 +0200

    functionally final version. still need some data & some tuning of looks :)

---
+++ ingredients.js
@@ -0,0 +1,322 @@
+cat_salad = ["veggies", "proteins", "cheese", "extra", "base"];
+cat_salad_fr = {"base": "Base", "veggies": "Légumes", "cheese": "Fromage", "proteins": "Protéines", "extra": "Extra"};
+cat_salad_en = {"base": "Base", "veggies": "Veggies", "cheese": "Cheese", "proteins": "Proteins", "extra": "Extra"};
+
+var salad_fr_veggies = [
+  "Tomate", 
+  "Avocat",
+  "Mangue", 
+  "Poivron", 
+  "Tomate séchée", 
+  "Carotte", 
+  "Maïs", 
+  "Pois chiches", 
+  "Endive", 
+  "Haricots rouges", 
+  "Betterave", 
+  "Champignons"
+];
+
+var salad_en_veggies = [
+  "Tomato",
+  "Avocado",
+  "Mango",
+  "Bell pepper",
+  "Dried tomato",
+  "Carrot",
+  "Corn",
+  "Chickpea",
+  "Chicory",
+  "Red beans",
+  "Beet",
+  "Mushroom"
+]
+
+var salad_fr_cheese = [
+  "Feta", 
+  "Gruyère", 
+  "Emmental", 
+  "Chèvre", 
+  "Luzerner", 
+  "Roquefort", 
+  "Mozzarella"
+];
+
+var salad_en_cheese = [
+  "Feta", 
+  "Gruyère", 
+  "Emmental", 
+  "Goat cheese", 
+  "Luzerner", 
+  "Roquefort", 
+  "Mozzarella"
+];
+
+var salad_fr_proteins = [
+  "Saucisse", 
+  "Bacon", 
+  "Thon", 
+  "Œuf", 
+  "Poulet", 
+  "Nuggets", 
+  "Boulettes", 
+  "Saumon", 
+  "Jambon", 
+  "Tofu", 
+  "Canard séché", 
+  "Gésiers", 
+  "Jambon cru"
+];
+
+var salad_en_proteins = [
+  "Sausage",
+  "Bacon",
+  "Tuna",
+  "Egg",
+  "Chicken",
+  "Nuggets",
+  "Meatballs",
+  "Salmon",
+  "Ham",
+  "Tofu",
+  "Dried duck meat",
+  "Gizzards",
+  "Cured ham"
+]
+
+var salad_fr_extra = [
+  "Croûtons", 
+  "Œuf dur", 
+  "Pignons"
+];
+
+var salad_en_extra = [
+  "Croutons",
+  "Boiled egg",
+  "Pine nuts"
+];
+
+var salad_fr_base = [
+  "Salade", 
+  "Pâtes", 
+  "Riz", 
+  "Blé", 
+  "Pommes de terre"
+]
+
+var salad_en_base = [
+  "Greens",
+  "Pasta",
+  "Rice",
+  "Wheat",
+  "Potato"
+]
+
+cat_pizza = ["veggies", "proteins", "cheese", "extra"];
+cat_pizza_fr = {"veggies": "Légumes", "cheese": "Fromage", "proteins": "Protéines", "extra": "Extra"};
+cat_pizza_en = {"veggies": "Veggies", "cheese": "Cheese", "proteins": "Proteins", "extra": "Extra"};
+var pizza_fr_veggies = [
+  "Tomate",
+  "Oignon",
+  "Roquette",
+]
+
+var pizza_en_veggies = [
+  "Tomato",
+  "Onion",
+  "Arugula"
+]
+
+var pizza_fr_proteins = [
+  "Bacon",
+  "Poulet",
+  "Chorizo",
+  "Thon",
+  "Saucisses"
+]
+
+var pizza_en_proteins = [
+  "Bacon",
+  "Chicken",
+  "Chorizo",
+  "Tuna",
+  "Sausage"
+]
+
+var pizza_fr_cheese = [
+  "Mozzarella",
+  "Feta",
+  "Bleu",
+  "Raclette",
+  "Emmental",
+  "Chèvre"
+]
+
+var pizza_en_cheese = [
+  "Mozzarella",
+  "Feta",
+  "Blue cheese",
+  "Raclette",
+  "Emmental",
+  "Goat cheese"
+]
+
+var pizza_fr_extra = [
+  "Œuf",
+  "Pignons",
+  "Piment"
+]
+
+var pizza_en_extra = [
+  "Egg",
+  "Pine nuts",
+  "Chili"
+]
+
+cat_pasta = ["veggies", "proteins", "cheese", "extra", "sauce"];
+cat_pasta_fr = {"veggies": "Légumes", "cheese": "Fromage", "proteins": "Protéines", "extra": "Extra", "sauce": "Sauce"};
+cat_pasta_en = {"veggies": "Veggies", "cheese": "Cheese", "proteins": "Proteins", "extra": "Extra", "sauce": "Sauce"};
+
+var pasta_fr_veggies = [
+  "Tomate",
+  "Oignon",
+  "Poivron",
+  "Céleri",
+  "Champignons"
+]
+
+var pasta_en_veggies = [
+  "Tomato",
+  "Onion",
+  "Bell pepper",
+  "Celery",
+  "Mushrooms"
+]
+
+var pasta_fr_proteins = [
+  "Lardons",
+  "Poulet",
+  "Chorizo",
+  "Thon",
+  "Saucisses",
+  "Saumon"
+]
+
+var pasta_en_proteins = [
+  "Bacon",
+  "Chicken",
+  "Chorizo",
+  "Tuna",
+  "Sausage",
+  "Salmon"
+]
+
+var pasta_fr_cheese = [
+  "Mozzarella",
+  "Feta",
+  "Bleu",
+  "Emmental",
+  "Chèvre",
+  "Gruyère",
+  "Parmesan",
+  "Gorgonzola"
+]
+
+var pasta_en_cheese = [
+  "Mozzarella",
+  "Feta",
+  "Bleu",
+  "Emmental",
+  "Goat cheese",
+  "Gruyère",
+  "Parmesan",
+  "Gorgonzola"
+]
+
+var pasta_fr_extra = [
+  "Œuf",
+  "Pignons",
+  "Piment",
+  "Estragon",
+  "Aneth"
+]
+
+var pasta_en_extra = [
+  "Egg",
+  "Pine nuts",
+  "Chilli",
+  "Tarragon",
+  "Dill"
+]
+
+var pasta_fr_sauce = [
+  "Sauce tomate",
+  "Pesto",
+  "Sauce arrabiata",
+  "Crème fraîche"
+]
+
+var pasta_en_sauce = [
+  "Tomato sauce",
+  "Pesto",
+  "Arrabiata sauce",
+  "Cream"
+]
+
+cat_risotto = ["veggies", "proteins", "cheese"];
+cat_risotto_fr = {"veggies": "Légumes", "cheese": "Fromage", "proteins": "Protéines"};
+cat_risotto_en = {"veggies": "Veggies", "cheese": "Cheese", "proteins": "Proteins"};
+
+var risotto_fr_veggies = [
+  "Tomate",
+  "Oignon",
+  "Poivron",
+  "Céleri rave",
+  "Champignons",
+  "Céleri branche"
+]
+
+var risotto_en_veggies = [
+  "Tomato",
+  "Onion",
+  "Bell pepper",
+  "Celeriac",
+  "Mushrooms",
+  "Celery"
+]
+
+var risotto_fr_proteins = [
+  "Lardons",
+  "Poulet",
+  "Chorizo",
+  "Saucisses"
+]
+
+var risotto_en_proteins = [
+  "Bacon",
+  "Chicken",
+  "Chorizo",
+  "Sausage"
+]
+
+var risotto_fr_cheese = [
+  "Mozzarella",
+  "Feta",
+  "Bleu",
+  "Emmental",
+  "Chèvre",
+  "Gruyère",
+  "Parmesan",
+  "Gorgonzola"
+]
+
+var risotto_en_cheese = [
+  "Mozzarella",
+  "Feta",
+  "Bleu",
+  "Emmental",
+  "Goat cheese",
+  "Gruyère",
+  "Parmesan",
+  "Gorgonzola"
+]
