db5735df604ee923c9bba2a3d214873b47cdc5de
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2011-05-27 16:26:30 +0200

    proper indenting

--- saladgenerator.js
+++ saladgenerator.js
@@ -21,83 +21,84 @@ function create_selector(){
   for (key in catnames){
     $("#selector"+current).append("<option value='"+key+"'>" + catnames[key] +"</option>");
   }
-				$("#selector"+current).change(function(event){
-					choose_element(event.target.id.replace("selector", ""));
-				});
-				$("#change"+current).click(function(event){
-					choose_element(event.target.id.replace("change", ""));
-				});
-				current++;
-			}
+  $("#selector"+current).change(function(event){
+    choose_element(event.target.id.replace("selector", ""));
+  });
+  $("#change"+current).click(function(event){
+    choose_element(event.target.id.replace("change", ""));
+  });
+  current++;
+}
 			
-      $(document).ready(function(){
-				$("#moar").click(create_selector);
-				$("#premix").click(premix);
-				$("#lucky").click(lucky);
-				$("input[type=radio]").click(redraw);
-        redraw();
-      }); 
+$(document).ready(function(){
+  $("#moar").click(create_selector);
+  $("#premix").click(premix);
+  $("#lucky").click(lucky);
+  $("input[type=radio]").click(redraw);
+  redraw();
+}); 
       
-      function redraw(){
-				for (var i = 0; i<current; i++){
-					$("#selector"+i).remove();
-					$("#change"+i).remove();
-					$("#result"+i).remove();
-					$("p").remove();
-				}
-				type = $("input[name=type-o-dish]:checked").val();
-				lang = $("input[name=lang]:checked").val();
-				categories = eval("cat_"+ type);
-				catnames = eval("cat_" + type + "_" + lang);
-				current = 0;
-				for (var i = 0; i< 5; i++) {
-					create_selector();
-				}
-			}
+function redraw(){
+  for (var i = 0; i<current; i++){
+    $("#selector"+i).remove();
+    $("#change"+i).remove();
+    $("#result"+i).remove();
+    $("p").remove();
+  }
+  type = $("input[name=type-o-dish]:checked").val();
+  lang = $("input[name=lang]:checked").val();
+  categories = eval("cat_"+ type);
+  catnames = eval("cat_" + type + "_" + lang);
+  current = 0;
+  for (var i = 0; i< 5; i++) {
+    create_selector();
+  }
+}
       
-      function premix(){
-				switch(type){
-					case("salad"):
-						$("#selector0").val("veggies");
-						$("#selector1").val("veggies");
-						$("#selector2").val("proteins");
-						$("#selector3").val("cheese");
-						$("#selector4").val("extra");
-						if($("#needbase").attr('checked')){
-							if(current <= 5){
-								create_selector();
-							}
-							$("#selector5").val("base");
-						}
-						break;
-					case("pizza"):
-					case("risotto"):
-						$("#selector0").val("veggies");
-						$("#selector1").val("veggies");
-						$("#selector2").val("veggies");
-						$("#selector3").val("cheese");
-						$("#selector4").val("proteins");
-						break;
-					case("pasta"):
-						$("#selector0").val("veggies");
-						$("#selector1").val("veggies");
-						$("#selector2").val("proteins");
-						$("#selector3").val("sauce");
-						$("#selector4").val("extra");
-						break;
-				}
-				fill_all();
-			}
-			function fill_all(){
-				for (var i = 0; i<current; i++){
-					choose_element(i);
-				}
-			}
-			function lucky(){
-				
-				for (var i = 0; i<current; i++){
-					var cat= categories[Math.floor(Math.random()*categories.length)];
-					$("#selector"+i).val(cat);
-				}
-				fill_all()
-			}
+function premix(){
+  switch(type){
+  case("salad"):
+    $("#selector0").val("veggies");
+    $("#selector1").val("veggies");
+    $("#selector2").val("proteins");
+    $("#selector3").val("cheese");
+    $("#selector4").val("extra");
+    if($("#needbase").attr('checked')){
+      if(current <= 5){
+        create_selector();
+      }
+      $("#selector5").val("base");
+    }
+    break;
+  case("pizza"):
+  case("risotto"):
+    $("#selector0").val("veggies");
+    $("#selector1").val("veggies");
+    $("#selector2").val("veggies");
+    $("#selector3").val("cheese");
+    $("#selector4").val("proteins");
+    break;
+  case("pasta"):
+    $("#selector0").val("veggies");
+    $("#selector1").val("veggies");
+    $("#selector2").val("proteins");
+    $("#selector3").val("sauce");
+    $("#selector4").val("extra");
+    break;
+  }
+  fill_all();
+}
+
+function fill_all(){
+  for (var i = 0; i<current; i++){
+    choose_element(i);
+  }
+}
+
+function lucky(){				
+  for (var i = 0; i<current; i++){
+    var cat= categories[Math.floor(Math.random()*categories.length)];
+    $("#selector"+i).val(cat);
+  }
+  fill_all()
+}
